a6f028a52042ee11f862ac651473d8aad19c8f7d
basicowy <buczkowskikamil638@gmail.com>
2026-04-29 18:06:11 +0200

    next update 2

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "default" {
-  name = "rg-crc2026-student-XXX-lab"
+  name = "rg-crc2026-student-206-lab"
 }
 
 data "azurerm_client_config" "current" {}
--- providers.tf
+++ providers.tf
@@ -6,26 +6,21 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.70.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
   }
   backend "azurerm" {
-    resource_group_name  = "rg_name"
-    storage_account_name = "sa_name"
-    container_name       = "container_name"
-    key                  = "lab06.terraform.tfstate"
+    resource_group_name  = "rg-crc2026-student-206-lab"
+    storage_account_name = "synapssatf"
+    container_name       = "terraform"
+    key                  = "terraform_new.tfstate"
   }
 }
 
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy    = true
+      purge_soft_delete_on_destroy    = false
       recover_soft_deleted_key_vaults = true
     }
   }
-  subscription_id                 = "ID_HERE"
   resource_provider_registrations = "none"
 }
