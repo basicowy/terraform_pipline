83059d5fa77e46f0e738739e9ce42847a8686833
basicowy <buczkowskikamil638@gmail.com>
2026-04-29 17:26:55 +0200

    Initial Terraform setup

---
+++ providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.10"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.70.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-crc2026-student-206-lab"
+    storage_account_name = "synapssatf"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+  resource_provider_registrations = "none"
+}
